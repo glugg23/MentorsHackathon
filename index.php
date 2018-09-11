afbef057f4e6c4200d3ed7b49dcd043456d3e8b9
glugg23 <m.leonhardt424@gmail.com>
2018-09-11 20:37:18 +0100

    Added basic rest command

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    require __DIR__ . '/vendor/autoload.php';
+
+    $api = new RestClient([
+        'base_url' => "https://api.teller.io",
+        'format' => "json",
+        'headers' => ['Authorization' => 'Bearer '.$api_key]
+    ]);
+
+    $result = $api->get('/accounts/'.$account_info.'/transactions');
+?>
