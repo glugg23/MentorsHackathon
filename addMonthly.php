25abb501f60ae4fb38ebbee7fa7a4eb02dd25e3f
glugg23 <m.leonhardt424@gmail.com>
2018-09-12 16:52:50 +0100

    added method for addMonthly

--- addMonthly.php
+++ addMonthly.php
@@ -1,3 +1,10 @@
 <?php
-    var_dump($_POST)
+    include_once 'PRIVATE.php';
+
+    $pdo = new PDO("mysql:host=".$host.";dbname=".$dbname, $username, $password);
+    $fluent = new FluentPDO($pdo);
+
+    $values = array('amount' => $_POST['amount'], 'description' => $_POST['name'], 'date' => $_POST['day']);
+
+    $query = %fluent->insertInto('monthlys', $values)->execute();
 ?>
