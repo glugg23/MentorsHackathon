762ed77d275f5ec2e90c3c56503dd3750583753d
glugg23 <m.leonhardt424@gmail.com>
2018-09-12 16:19:31 +0100

    array_slice fix

--- index.php
+++ index.php
@@ -64,10 +64,8 @@
             <div class="row">
               <?php
                   $result = $api->get('/accounts/'.$account_info.'/transactions');
-
-                  foreach ($result as $key => $value) { 
-                    $value = array_slice($value, 0, 20);
-                    ?>
+                  $array = array_slice($result, 0, 20);
+                  foreach ($result as $key => $value) { ?>
                     <div class="col-3 <?= ($value->type == "credit" || $value->type == "transfer") ? 'monthlys-positive' : 'monthlys-negative' ?>">
                       <?= (($value->amount > 0) ? '+&pound;' : '-&pound;').floor(abs($value->amount)); ?>
                     </div>
