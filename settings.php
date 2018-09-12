0195b768a8668c98ad5a904d8eaa293a37354a29
Barrow <40337360@live.napier.ac.uk>
2018-09-12 16:31:16 +0100

    changed button

--- settings.php
+++ settings.php
@@ -114,7 +114,7 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <input type="submit" class="btn btn-primary">Save changes</input>
       </div>
     </div>
   </div>
