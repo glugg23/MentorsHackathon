d67224913721dd011a2b937770e2ed527045120f
glugg23 <m.leonhardt424@gmail.com>
2018-09-12 12:20:01 +0100

    Found out how to seperate the result into individual parts

--- index.php
+++ index.php
@@ -5,10 +5,12 @@
 
     $api = new RestClient([
         'base_url' => "https://api.teller.io",
-        'headers' => ['Authorization' => 'Bearer '.$api_key]
+        'headers' => ['Authorization' => 'Bearer '.$api_key],
     ]);
 
     $result = $api->get('/accounts/'.$account_info.'/transactions');
 
-    var_dump($result);
+    foreach ($result as $key => $value) {
+        echo $value->description;
+    }
 ?>
